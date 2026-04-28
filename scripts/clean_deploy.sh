4a09301191c50d9852d52e589ba1bff2877c6315
Aires <aires@vm01.airespaulino.com.br>
2026-04-28 14:29:02 -0300

    first commit

---
+++ scripts/clean_deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rfv secrets
+rm -rfv volumes
+rm -rfv .env
+rm -rfv Dockerfile
--- scripts/first_deploy.sh
+++ scripts/first_deploy.sh
@@ -6,12 +6,15 @@ PROJECT_DIR="$PWD"
 SUPERSET_EXAMPLE_ENV="$PROJECT_DIR/superset_env_example.conf"
 SUPERSET_ENV="$PROJECT_DIR/.env"
 
+SUPERSET_DOCKERFILE_EXAMPLE="$PROJECT_DIR/Dockerfile.example"
+SUPERSET_DOCKERFILE="$PROJECT_DIR/Dockerfile"
+
 SUPERSET_DBPASS=$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
 SUPERSET_ADMIN_PASS=$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
 SUPERSET_SECRET=$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 64)
 SUPERSET_VERSION=5.0.0
 
-mkdir -p secrets 
+#mkdir -p secrets
 mkdir -p volumes/{db_data,redis,superset_home,superset_config,superset_docker}
 
 echo "Pasta do projeto: $PROJECT_DIR"
@@ -22,8 +25,8 @@ echo "Senha api: $SUPERSET_SECRET"
 echo "Informe o nome do Cliente:"
 read -r SUPERSET_CLIENTE
 
-#echo "Informe a tag da versão desejada:"
-#read -r SUPERSET_VERSION
+echo "Informe a tag da versão desejada:"
+read -r SUPERSET_VERSION
 
 cp "$SUPERSET_EXAMPLE_ENV" "$SUPERSET_ENV"
 
@@ -31,4 +34,7 @@ sed -i "s/superset_db_password/$SUPERSET_DBPASS/g" "$SUPERSET_ENV"
 sed -i "s/superset_admin_password/$SUPERSET_ADMIN_PASS/g" "$SUPERSET_ENV"
 sed -i "s/SUPERSET_SUPER_SECRET_KEY/$SUPERSET_SECRET/g" "$SUPERSET_ENV"
 sed -i "s/superset_cliente/$SUPERSET_CLIENTE/g" "$SUPERSET_ENV"
-sed -i "s/superset_version/$SUPERSET_VERSION/g" "$SUPERSET_ENV"
+
+cp "$SUPERSET_DOCKERFILE_EXAMPLE" "$SUPERSET_DOCKERFILE"
+
+sed -i "s/superset_version/$SUPERSET_VERSION/g" "$SUPERSET_DOCKERFILE"
